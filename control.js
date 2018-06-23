75aa29a428d0f0472cdb61b8553578e1548a025f
thuygiang <lethuygiang2309@gmail.com>
2018-06-23 14:54:40 +0700

    some message

--- control.js
+++ control.js
@@ -2,23 +2,23 @@
 const app_11 = new Vue({
       el : "#app_11", 
       data  :{ 
-          mode: 'Loading...', 
+          sth: 'Loading...', 
        },
       create: function () {
         this.getMode();
       },
       methods: {
          getMode: function() {
-  url = 'http://demo-arisite.net:3000/sensors';
+  url = 'http://demo-arisite.net:3000/status/get';
    var app=this;
   axios.get(url
   ).then(function(response) {
-    app.mode = response.data.mode; 
-    if(app.mode==1) {
-      app.mode='MANUAL';
+    app.sth = response.data.sth; 
+    if(app.sth==true) {
+      app.sth='START';
     }
     else {
-      app.mode='AUTO';
+      app.sth='OFF';
     }
     }).catch(function(err){
     console.log(err);
@@ -28,28 +28,29 @@ const app_11 = new Vue({
 });
 setInterval(() => {
   app_11.getMode();
-},5*1000);
+},3*1000);
 //////////////////////////////
 const app_22 = new Vue({
       el : "#app_22", 
       data  :{ 
-          pump1s: 'Loading...', 
+          pump1c: 'Loading...', 
        },
       create: function () {
         this.getPump1s();
       },
       methods: {
          getPump1s: function() {
+          console.log('a');
   url = 'http://demo-arisite.net:3000/status/get';
    var app=this;
   axios.get(url
   ).then(function(response) {
-    app.pump1s = response.data.pump1s; 
-    if(app.pump1s==true) {
-      app.pump1s='ON';
+    app.pump1c = response.data.pump1c; 
+    if(app.pump1c==true) {
+      app.pump1c='ON';
     }
     else {
-      app.pump1s='OFF';
+      app.pump1c='OFF';
     }
     }).catch(function(err){
     console.log(err);
@@ -59,12 +60,12 @@ const app_22 = new Vue({
 });
 setInterval(() => {
   app_22.getPump1s();
-},5*1000);
+},3*1000);
 ///////////////////////////////
 const app_33 = new Vue({
       el : "#app_33", 
       data  :{ 
-          pump2s: 'Loading...', 
+          pump2c: 'Loading...', 
        },
       create: function () {
         this.getPump2s();
@@ -75,12 +76,12 @@ const app_33 = new Vue({
    var app=this;
   axios.get(url
   ).then(function(response) {
-    app.pump2s = response.data.pump2s; 
-    if(app.pump2s==true) {
-      app.pump2s='ON';
+    app.pump2c = response.data.pump2c; 
+    if(app.pump2c==true) {
+      app.pump2c='ON';
     }
     else {
-      app.pump2s='OFF';
+      app.pump2c='OFF';
     }
     }).catch(function(err){
     console.log(err);
@@ -90,28 +91,296 @@ const app_33 = new Vue({
 });
 setInterval(() => {
   app_33.getPump2s();
-},5*1000);
+},3*1000);
 ///////////////////
-var mqtt;
-   var reconnectTimeout=2000;
-   var host="broker.mqttdashboard.com";
-   var port=8000;
+// var mqtt;
+//    var reconnectTimeout=2000;
+//    var host="broker.mqttdashboard.com";
+//    var port=8000;
 
-   function onConnect(){
-    console.log("Connected");
-    message = new Paho.MQTT.Message("asdasd");
-    message.destinationName ="sensor1";
-    mqtt.send(message);
-    console.log("Success!! Topic: " + message.destinationName + " Message: " + message  )
-   }
+//    function onConnect(){
+//     console.log("Connected");
+//     message = new Paho.MQTT.Message("asdasd");
+//     message.destinationName ="sensor1";
+//     mqtt.send(message);
+//     console.log("Success!! Topic: " + message.destinationName + " Message: " + message  )
+//    }
 
-   function MQTTconnect(){
-    console.log("connecting to " + host + " " + port);
-    mqtt = new Paho.MQTT.Client(host,port,"clientjs");
-     var options = {
-      timeout: 3,
-      onSuccess: onConnect,
-     };
-    mqtt.connect(options); 
-   }
-   MQTTconnect();
+//    function MQTTconnect(){
+//     console.log("connecting to " + host + " " + port);
+//     mqtt = new Paho.MQTT.Client(host,port,"clientjs");
+//      var options = {
+//       timeout: 3,
+//       onSuccess: onConnect,
+//      };
+//     mqtt.connect(options); 
+//    }
+//    MQTTconnect();
+//////////////////////////////////////////////////////
+  $('#bt1').click(function(e){
+        e.preventDefault();
+
+         $.ajax({
+            type: "get",
+            url: "http://demo-arisite.net:3000/status/get",
+            // data: modeData,
+            contentType: "application/json",
+            success: function(response) {
+              var flag = 0;
+
+                if(response.sth ==false ){
+
+                  $.ajax({
+                          type: "get",
+                          url: "http://demo-arisite.net:3000/status/create",
+                          data: {
+                            sth: true,
+                            pump1c: response.pump1c,
+                            pump2c: response.pump2c
+                          },
+                          // dataType: "json",
+                          contentType: "application/json",
+                          success: function(response) {
+                            alert("done");
+
+
+                          },
+
+                          error: function (error) {console.log(error);}
+                       });
+              }
+              else {
+                alert("Ready");
+              }
+           },
+
+            error: function (error) {
+              console.log(error); 
+            }
+        
+      });
+     });
+////////////////////////////////
+$('#bt3').click(function(e){
+        e.preventDefault();
+      
+
+         $.ajax({
+            type: "get",
+            url: "http://demo-arisite.net:3000/status/get",
+            // data: modeData,
+            contentType: "application/json",
+            success: function(response) {
+              var flag = 0;
+
+                if(response.pump1c ==false && response.sth==true){
+
+                  $.ajax({
+                          type: "get",
+                          url: "http://demo-arisite.net:3000/status/create",
+                          data: {
+                            sth: response.sth,
+                            pump1c: true,
+                            pump2c: response.pump2c
+                          },
+                          // dataType: "json",
+                          contentType: "application/json",
+                          success: function(response) {
+                            alert("done");
+
+
+                          },
+
+                          error: function (error) {console.log(error);}
+                       });
+              }
+              else {
+                alert("Ready");
+              }
+           },
+
+            error: function (error) {
+              console.log(error); 
+            }
+        
+      });
+     });
+//////////////////////////////////
+$('#bt4').click(function(e){
+        e.preventDefault();
+      
+
+         $.ajax({
+            type: "get",
+            url: "http://demo-arisite.net:3000/status/get",
+            // data: modeData,
+            contentType: "application/json",
+            success: function(response) {
+              var flag = 0;
+
+                if(response.pump1c ==true && response.sth==true){
+
+                  $.ajax({
+                          type: "get",
+                          url: "http://demo-arisite.net:3000/status/create",
+                          data: {
+                            sth: response.sth,
+                            pump1c: false,
+                            pump2c: response.pump2c
+                          },
+                          // dataType: "json",
+                          contentType: "application/json",
+                          success: function(response) {
+                            alert("done");
+
+
+                          },
+
+                          error: function (error) {console.log(error);}
+                       });
+              }
+              else {
+                alert("Ready");
+              }
+           },
+
+            error: function (error) {
+              console.log(error); 
+            }
+        
+      });
+     });
+//////////////////////////////////////////
+$('#bt5').click(function(e){
+        e.preventDefault();
+      
+
+         $.ajax({
+            type: "get",
+            url: "http://demo-arisite.net:3000/status/get",
+            // data: modeData,
+            contentType: "application/json",
+            success: function(response) {
+              var flag = 0;
+
+                if(response.pump2c ==false && response.sth==true){
+
+                  $.ajax({
+                          type: "get",
+                          url: "http://demo-arisite.net:3000/status/create",
+                          data: {
+                            sth: response.sth,
+                            pump1c: response.pump1c,
+                            pump2c: true
+                          },
+                          // dataType: "json",
+                          contentType: "application/json",
+                          success: function(response) {
+                            alert("done");
+
+
+                          },
+
+                          error: function (error) {console.log(error);}
+                       });
+              }
+              else {
+                alert("Ready");
+              }
+           },
+
+            error: function (error) {
+              console.log(error); 
+            }
+        
+      });
+     });
+/////////////////////////////////////////
+$('#bt6').click(function(e){
+        e.preventDefault();
+      
+
+         $.ajax({
+            type: "get",
+            url: "http://demo-arisite.net:3000/status/get",
+            // data: modeData,
+            contentType: "application/json",
+            success: function(response) {
+              var flag = 0;
+
+                if(response.pump2c ==true && response.sth==true){
+
+                  $.ajax({
+                          type: "get",
+                          url: "http://demo-arisite.net:3000/status/create",
+                          data: {
+                            sth: response.sth,
+                            pump1c: response.pump1c,
+                            pump2c: false
+                          },
+                          // dataType: "json",
+                          contentType: "application/json",
+                          success: function(response) {
+                            alert("done");
+
+
+                          },
+
+                          error: function (error) {console.log(error);}
+                       });
+              }
+              else {
+                alert("Ready");
+              }
+           },
+
+            error: function (error) {
+              console.log(error); 
+            }
+        
+      });
+     });
+//////////////////////////
+$('#bt2').click(function(e){
+        e.preventDefault();
+
+         $.ajax({
+            type: "get",
+            url: "http://demo-arisite.net:3000/status/get",
+            // data: modeData,
+            contentType: "application/json",
+            success: function(response) {
+              var flag = 0;
+
+                if(response.sth ==true ){
+
+                  $.ajax({
+                          type: "get",
+                          url: "http://demo-arisite.net:3000/status/create",
+                          data: {
+                            sth: false,
+                            pump1c: false,
+                            pump2c: false
+                          },
+                          // dataType: "json",
+                          contentType: "application/json",
+                          success: function(response) {
+                            alert("done");
+
+
+                          },
+
+                          error: function (error) {console.log(error);}
+                       });
+              }
+              else {
+                alert("Ready");
+              }
+           },
+
+            error: function (error) {
+              console.log(error); 
+            }
+        
+      });
+     });
